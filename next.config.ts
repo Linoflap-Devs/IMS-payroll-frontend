c62272c75b898a0efae86e490703b57fe07cc7f3
WRavagoLFT <wendell.ravago@linoflaptech.com>
2025-05-16 10:37:21 +0800

    add ignore during builds to tsconfig

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,9 @@ const nextConfig: NextConfig = {
     // number of pages that should be kept simultaneously without being disposed
     pagesBufferLength: 2,
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
  
 };
 
