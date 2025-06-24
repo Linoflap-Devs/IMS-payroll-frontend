0399cefa8814e8697e271556643dd17d48478507
WRavagoLFT <wendell.ravago@linoflaptech.com>
2025-06-24 15:53:19 +0800

    update utils to add truncateText and formatCurrency

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,23 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function truncateText(text: string, maxLength: number) {
+  if (!text || typeof text !== 'string') {
+    return '';
+  }
+  
+  if (text.length <= maxLength) {
+    return text;
+  }
+  
+  return text.substring(0, maxLength) + '...';
+}
+
+export function formatCurrency(amount: number | string): string {
+    if (typeof(amount) == 'string') return amount
+    return new Intl.NumberFormat('en-US', {
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    }).format(amount);
+}
